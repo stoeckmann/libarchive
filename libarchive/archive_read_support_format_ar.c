7054bec5433f05ae0f8249f67b1cb6ac54e3f577
Tobias Stoeckmann <tobias@stoeckmann.org>
2026-06-28 11:51:52 +0200

    ar: Simplify error handling in _ar_read_header
    The comment states that a warning should be returned, but
    97f964e3e0ce3ae34bfb4c366a37ba7c0d9610a6 explicitly changed that to
    fatal. Adjust the comment and remove unneeded function call.
    
    Signed-off-by: Tobias Stoeckmann <tobias@stoeckmann.org>

--- libarchive/archive_read_support_format_ar.c
+++ libarchive/archive_read_support_format_ar.c
@@ -312,14 +312,10 @@ _ar_read_header(struct archive_read *a, struct archive_entry *entry,
 	 */
 	if (filename[0] == '/' && filename[1] >= '0' && filename[1] <= '9') {
 		number = ar_atol10(h + AR_name_offset + 1, AR_name_size - 1);
-		/*
-		 * If we can't look up the real name, warn and return
-		 * the entry with the wrong name.
-		 */
+		/* Fail if we can't look up the real name. */
 		if (ar->strtab == NULL || number >= ar->strtab_size) {
 			archive_set_error(&a->archive, EINVAL,
 			    "Can't find long filename for GNU/SVR4 archive entry");
-			archive_entry_copy_pathname(entry, filename);
 			return (ARCHIVE_FATAL);
 		}
 
